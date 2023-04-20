554dccd90c724a8acdf5497f444a623d7c317312
Shakhron <sokers30@gmail.com>
2023-04-20 23:39:07 +0300

    last

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'dart:developer';
-
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,12 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
           body: Center(
         child: CheckBoxWithTextWidget(
           text: 'texttextteadadafxt',
@@ -27,89 +23,59 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class CheckBoxWithTextWidget extends StatefulWidget {
+class CheckBoxWithTextWidget extends StatelessWidget {
   final String text;
   final bool isSelected;
 
   const CheckBoxWithTextWidget({
-    super.key,
+    Key? key,
     required this.text,
     required this.isSelected,
-  });
-
-  @override
-  State<CheckBoxWithTextWidget> createState() => _CheckBoxWithTextWidgetState();
-}
-
-class _CheckBoxWithTextWidgetState extends State<CheckBoxWithTextWidget> {
-  var textVisible = ValueNotifier<bool>(true);
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    log(textVisible.toString());
     return Row(
       children: [
-        ValueListenableBuilder(
-            valueListenable: textVisible,
-            builder: (context, value, w) {
-              if (value) {
-                return Text(widget.text);
-              } else {
-                return Flexible(
-                  child: Text(
-                    widget.text,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                );
-              }
-            }),
-        DashWidget(textVisible: textVisible),
+        Flexible(
+            flex: 2,
+            child: Text(
+              text,
+              overflow: TextOverflow.ellipsis,
+            )),
+        Expanded(
+          flex: 8,
+          child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              final boxWidth = constraints.constrainWidth();
+              const dashWidth = 15.0;
+              final dashCount = (boxWidth / (8 * dashWidth)).floor();
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: List.generate(dashCount, (_) {
+                  return (boxWidth <= 10)
+                      ? const SizedBox()
+                      : const SizedBox(
+                          width: dashWidth,
+                          height: 1,
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(color: Colors.black),
+                          ),
+                        );
+                }),
+              );
+            },
+          ),
+        ),
         SizedBox(
           width: 24,
           height: 24,
           child: Checkbox(
-            value: widget.isSelected,
+            value: isSelected,
             onChanged: (value) {},
           ),
         ),
       ],
-    );
-  }
-}
-
-class DashWidget extends StatelessWidget {
-  final ValueNotifier<bool> textVisible;
-  const DashWidget({super.key, required this.textVisible});
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          final boxWidth = constraints.constrainWidth();
-          const dashWidth = 5.0;
-          final dashCount = (boxWidth / (2 * dashWidth)).floor();
-          if (dashCount <= 2) {
-            textVisible.value = false;
-          } else {
-            textVisible.value = true;
-          }
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: List.generate(dashCount, (_) {
-              return SizedBox(
-                width: dashWidth,
-                height: 1,
-                child: (textVisible.value)
-                    ? const DecoratedBox(
-                        decoration: BoxDecoration(color: Colors.black),
-                      )
-                    : const SizedBox(),
-              );
-            }),
-          );
-        },
-      ),
     );
   }
 }
